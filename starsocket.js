630f1f34d852d89c0021f9f00d5f98f4e5fc33e4
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-02-13 13:30:10 +0100

    Update starsocket.js

--- starsocket.js
+++ starsocket.js
@@ -1,32 +1,12 @@
-const { Server } = require("socket.io");
-
-const io = new Server(8080, { /* options */ });
-
-
-
+const io = require("socket.io") (8080);
 console.log("Server started")
+
 
 io.on("connection", (socket) => {
   // ...
   console.log("Connection Successfull")
+  
   console.log(socket.id); // ojIckSD2jqNzOqIrAGzL
-
-  socket.emit("hello", "StarDash");
-
-  /* socket.data.username = "StarrrDash";
-
-  console.log(socket.rooms); // Set { <socket.id> }
-  socket.join("room1");
-  console.log(socket.rooms); // Set { <socket.id>, "room1" }
-
-  socket.on("private message", (anotherSocketId, msg) => {
-    socket.to(anotherSocketId).emit("private message", socket.id, msg);
-  });
-  */
-
-    socket.on("hello", (arg) => {
-      console.log(arg); // world
-    });
  
 
 });
