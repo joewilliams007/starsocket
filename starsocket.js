7794ba80f882a362f776f3682027bed1e682b939
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-02-13 13:36:08 +0100

    Update starsocket.js

--- starsocket.js
+++ starsocket.js
@@ -1,11 +1,15 @@
-const io = require("socket.io") (8080);
+const io = require("socket.io") (3000, {
+  cors: {
+    origin: ["http://localhost:8080"],
+  },
+});
 console.log("Server started")
 
 
 io.on("connection", (socket) => {
   // ...
   console.log("Connection Successfull")
-  
+
   console.log(socket.id); // ojIckSD2jqNzOqIrAGzL
  
 
