0c2e6c5fecfa8b5b009b83df9a16e11d9a3b0df6
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-02-14 18:32:01 +0100

    eeh

--- starsocket.js
+++ starsocket.js
@@ -1,21 +1,17 @@
 var net = require('net');
 let fs = require('fs')
 const _messages = JSON.parse(fs.readFileSync('./messages.json'));
-
 var server = net.createServer();
+console.log('Started Server.');
 
 var server = net.createServer(function(socket) {
-	
 	console.log('A new connection has been established.');
-	socket.write('Echo server\r\n');
 	socket.pipe(socket);
 	socket.on('data', function(chunk) {
 		console.log(`Data received from client: ${chunk.toString()}`);
-	
 			  //-- Save Message         		
 			  _messages.push(chunk.toString().replace(/\n/g, " "))
 			  fs.writeFileSync('./messages.json', JSON.stringify(_messages))
-	
 	});
 	
 	// When the client requests to end the TCP connection with the server, the server
@@ -23,7 +19,6 @@ var server = net.createServer(function(socket) {
 	socket.on('end', function() {
 		console.log('Closing connection with the client');
 	});
-	
 	// Don't forget to catch error, for your own sake.
 	socket.on('error', function(err) {
 		console.log(`Error: ${err}`);
@@ -31,11 +26,6 @@ var server = net.createServer(function(socket) {
 });
 
 
- // The server can also receive data from the client by reading from its socket.
-
-
-
-server.listen(12345);
-
+server.listen(2222);
 
 
