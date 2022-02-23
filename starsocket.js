34e50c0a9302ba517ab13872204fb3ed389282a4
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-02-23 02:21:51 +0100

    Update starsocket.js

--- starsocket.js
+++ starsocket.js
@@ -88,8 +88,33 @@ var server = net.createServer(function(socket) {
 
 	socket.on('data', function(chunk) {
 		
-		console.log(request.connection.remoteAddress);
-		
+		console.log('---------server details -----------------');
+
+		var address = server.address();
+		var port = address.port;
+		var family = address.family;
+		var ipaddr = address.address;
+		console.log('Server is listening at port' + port);
+		console.log('Server ip :' + ipaddr);
+		console.log('Server is IP4/IP6 : ' + family);
+	  
+		var lport = socket.localPort;
+		var laddr = socket.localAddress;
+		console.log('Server is listening at LOCAL port' + lport);
+		console.log('Server LOCAL ip :' + laddr);
+	  
+		console.log('------------remote client info --------------');
+	  
+		var rport = socket.remotePort;
+		var raddr = socket.remoteAddress;
+		var rfamily = socket.remoteFamily;
+	  
+		console.log('REMOTE Socket is listening at port' + rport);
+		console.log('REMOTE Socket ip :' + raddr);
+		console.log('REMOTE Socket is IP4/IP6 : ' + rfamily);
+	  
+		console.log('--------------------------------------------')
+
 		if (chunk.toString().includes("clear")) {
 			
 			exec(`rm -rf ./messages.json`);
