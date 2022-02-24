d450ce9bf99c45904290f6cb9be83391c4892626
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-02-24 18:33:30 +0100

    Update starsocket.js

--- starsocket.js
+++ starsocket.js
@@ -8,27 +8,7 @@ var server = net.createServer();
 console.log('Started Server.\nAll Saved messages: '+ count1);
 
 
-// function to encode file data to base64 encoded string
-file = "./image.jpg"
-function base64_encode(file) {
-    // read binary data
-    var bitmap = fs.readFileSync(file);
-    // convert binary data to base64 encoded string
-    return new Buffer.from(bitmap).toString('base64');
-}
 
-// function to create file from base64 encoded string
-function base64_decode(base64str, file) {
-    // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
-    var bitmap = new Buffer.from(base64str, 'base64');
-    // write buffer to file
-    fs.writeFileSync(file, bitmap);
-    console.log('******** File created from base64 encoded string ********');
-}
-
-// convert image to base64 encoded string
-var base64str = base64_encode('image.jpg');
-console.log(base64str);
 console.log("finished");
 
 var server = net.createServer(function(socket) {
