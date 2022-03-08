4dfe401c0af24d085969bc4a4ac8f4897b0fed12
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-03-08 19:39:41 +0100

    Create StarSocketMessageSendRegistrationServer.js

---
+++ servers/StarSocketMessageSendRegistrationServer.js
@@ -0,0 +1,40 @@
+var net = require('net');
+let fs = require('fs');
+
+
+var port = 2228
+var server = net.createServer();
+
+var server = net.createServer(function(socket) {
+	var _messages = JSON.parse(fs.readFileSync('messagesRegistration.json')).reverse();
+
+	var entireMessage = ""
+
+	for (i = 0; i < 60; i++){
+		entireMessage += _messages[i] + "\n"
+	}
+
+	entireMessage += "Total messages: "+ _messages.length
+
+    socket.write(entireMessage.replace(/undefined/g,""))
+    socket.end();
+
+	serverInfo('A new connection has been established.');
+	
+	socket.on('end', function() {
+		serverInfo('Message sent. Closing connection with the client')
+		socket.destroy()
+	});
+
+	socket.on('error', function(err) {
+		socket.destroy()
+	});
+});
+
+
+function serverInfo(info){
+	console.log("-> @Message Send Server: " + info)
+}
+
+server.listen(port);
+serverInfo("Started server on port: " + port)
