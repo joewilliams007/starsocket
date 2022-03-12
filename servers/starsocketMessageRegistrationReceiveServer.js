5559fd8c6278ffe53ad3ab6d0e1f7a552b8a7394
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-03-12 16:47:48 +0100

    Update starsocketMessageRegistrationReceiveServer.js

--- servers/starsocketMessageRegistrationReceiveServer.js
+++ servers/starsocketMessageRegistrationReceiveServer.js
@@ -93,7 +93,7 @@ var server = net.createServer(function(socket) {
 	var usernamesearch = data[1]
 	
 		var all = null
-		try {
+
 			const allFolder = './usernames/';
 			
 			fs.readdirSync(allFolder).forEach(file => {
@@ -107,9 +107,6 @@ var server = net.createServer(function(socket) {
 			});
 			console.log(`All files\n`+all)
 
-		} catch (e) {
-			console.log("ERROR SEARCHING")
-		}
 
 		} else if (receivedMessage.includes("confirmation")) {
 //Confirm Transfer ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
