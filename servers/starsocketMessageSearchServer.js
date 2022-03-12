d4a3f2d96636c5c6102d509c128df74065f560a8
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-03-12 16:34:41 +0100

    up

---
+++ servers/starsocketMessageSearchServer.js
@@ -0,0 +1,39 @@
+var net = require('net');
+let fs = require('fs');
+
+
+var port = 2229
+var server = net.createServer();
+
+var server = net.createServer(function(socket) {
+	var _messages = JSON.parse(fs.readFileSync('allusers.json')).reverse();
+
+	var entireMessage = ""
+
+	//for (i = 0; i < 60; i++){
+	//
+	//}
+	var entireMessage = _messages[0]
+
+    socket.write(entireMessage.replace(/undefined/g,""))
+    socket.end();
+
+	serverInfo('A new connection has been established.');
+	
+	socket.on('end', function() {
+		serverInfo('Message sent. Closing connection with the client')
+		socket.destroy()
+	});
+
+	socket.on('error', function(err) {
+		socket.destroy()
+	});
+});
+
+
+function serverInfo(info){
+	console.log("-> @Message SendSearch Server: " + info)
+}
+
+server.listen(port);
+serverInfo("Started server on port: " + port)
