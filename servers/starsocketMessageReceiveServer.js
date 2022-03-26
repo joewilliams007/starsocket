985609f0615066e554ba81f1520031fdb0da7fb5
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-03-27 00:22:44 +0100

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -1,11 +1,39 @@
 var net = require('net');
 let fs = require('fs');
 const _messages = JSON.parse(fs.readFileSync('messages.json'));
-
+// Server Ports------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 var port = 2224;
 var server = net.createServer();
+// DataBase ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+const mysql = require('mysql');
 
+// First you need to create a connection to the database
+// Be sure to replace 'user' and 'password' with the correct values
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'user',
+  password: 'password',
+});
 
+con.connect((err) => {
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+con.query('SELECT * FROM authors', (err,rows) => {
+	if(err) throw err;
+  
+	console.log('Data received from Db:');
+	console.log(rows);
+  });
+con.end((err) => {
+  // The connection is terminated gracefully
+  // Ensures all remaining queries are executed
+  // Then sends a quit packet to the MySQL server.
+});
+// StartServer ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 var server = net.createServer(function(socket) {
 
 	console.log('--------------------------------------------')
