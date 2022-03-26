ca0a1fb7ecf292de9527a83634c1608889271700
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-03-26 22:15:42 +0100

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -8,9 +8,8 @@ var server = net.createServer();
 
 var server = net.createServer(function(socket) {
 
-	serverInfo('A new connection has been established.');
 	console.log('--------------------------------------------')
-
+	serverInfo('A new connection has been established.');
 	
     var receivedMessage = ""
 
@@ -23,9 +22,11 @@ var server = net.createServer(function(socket) {
 		serverInfo("size of received message string: " + receivedMessage.length.toString())
 		if(receivedMessage.length > 0){
 			serverInfo("received message: " + receivedMessage)
+			
 		}
 		serverInfo('Closing connection with the client')
-
+		console.log('--------------------------------------------')
+		
 		 //-- Save Message         		
 		 _messages.push(receivedMessage.toString())
 		 fs.writeFileSync('./messages.json', JSON.stringify(_messages))
