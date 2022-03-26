83704e02325d84fe49aaeaef2a6498b4b4518ce4
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-03-26 23:52:28 +0100

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -31,7 +31,7 @@ var server = net.createServer(function(socket) {
 		 _messages.push(receivedMessage.toString())
 		 fs.writeFileSync('./messages.json', JSON.stringify(_messages))
 
-		 switch(receivedMessage.toString().split(" ")) {
+		 switch(receivedMessage.toString()) {
 			case "hi":
 			  serverInfo("case was hi")
 			  break;
