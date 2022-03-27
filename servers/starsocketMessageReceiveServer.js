3db940c90b730434a285ed187c585db41643b4cd
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-03-27 19:00:22 +0200

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -59,7 +59,7 @@ case "register":
 		// args 2 will be password
 		// id will be set by database itself!
 		
-		connection.query(`SELECT * FROM Users WHERE username = ${args[1]}`, function (error, results, fields) {
+		connection.query(`SELECT * FROM Users WHERE username = "${args[1]}"`, function (error, results, fields) {
 			if (error) throw error;
 			console.log('Deleted all right?: ', results);
 		});
