6386a59441331e09a6b4d47d0781de90054bc3e9
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-03-27 14:40:58 +0200

    s

--- mysql.js
+++ mysql.js
@@ -2,7 +2,7 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'me',
+  user     : 'root',
   password : 'secret',
   database : 'my_db'
 });
