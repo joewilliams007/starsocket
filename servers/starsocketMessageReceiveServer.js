bc7b06a1ca0872f83d4d00c0a5dbc3fa5a6a84ef
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-03-27 20:06:02 +0200

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -84,7 +84,7 @@ case "register":
 
 		, function (error, results, fields) {
 			if (error) throw error;
-			console.log('Just logging >_< ', results.RowDataPacket[0].user_id);
+			console.log('Just logging >_< ', results.user_id);
 		});
 
 		id = "1";
