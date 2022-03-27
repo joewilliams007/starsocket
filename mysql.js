c063596e4585efa09ad9bb43750a6a19fe9a484b
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-03-27 14:47:18 +0200

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'secret',
-  database : 'my_db'
+  database : 'db_main'
 });
  
 connection.connect();
