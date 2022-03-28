53a2a6901c2a42543e43ec4e9acc31a6e696122a
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-03-28 18:35:24 +0200

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -104,7 +104,7 @@ case "login":
 	connection.query( // get the users stuff
 
 		`SELECT * FROM Users
-		WHERE user_id="${args[1]}" AND password = "${args[2]}" AND email= "${args[3]}"`
+		WHERE user_id="${args[1]}" AND password = "${args[2]}"`
 
 		, function (error, results, fields) {
 			if (error) serverInfo(error.message);
