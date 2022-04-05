32b089cc74fdce9d5f56a697a56208bf39190abf
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-04-05 17:50:06 +0200

    Update starsocket.js

--- starsocket.js
+++ starsocket.js
@@ -1,9 +1,8 @@
 console.log('-> S T A R T I N G  >_<');
 console.log(' ');
 
-var fileSendServer = require("./servers/starsocketFileSendServer.js")
-
-var fileReceiveServer = require("./servers/starsocketFileReceiveServer.js")
+// var fileSendServer = require("./servers/starsocketFileSendServer.js")
+// var fileReceiveServer = require("./servers/starsocketFileReceiveServer.js")
 var messageSendServer = require("./servers/starsocketMessageSendServer.js")
 var messageReceiveServer = require("./servers/starsocketMessageReceiveServer.js")
 
