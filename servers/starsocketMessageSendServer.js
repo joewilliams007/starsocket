71db9111b2311cf7b17e55f836e1096a5585e08c
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-04-09 15:36:58 +0200

    Update starsocketMessageSendServer.js

--- servers/starsocketMessageSendServer.js
+++ servers/starsocketMessageSendServer.js
@@ -25,11 +25,16 @@ var server = net.createServer(function(socket) {
 	entireMessage += "Total messages: "+ _messages.length
 
 	var lastOfFile = _messages[_messages.length-1]
-	serverInfo("last in file is "+lastOfFile)
+	// serverInfo("last in file is "+lastOfFile)
 	if (lastOfFile.split(" ",2)[0].includes(socket.remoteAddress)){
 		serverInfo("valid ip! "+lastOfFile+" to ip of "+socket.remoteAddress)
 		socket.write(removeFirstWord(lastOfFile))
+
+		if (removeFirstWord(lastOfFile).length>20){
+			serverInfo("writing sth over 20 characters") 
+		} else {
 		serverInfo("writing "+removeFirstWord(lastOfFile))
+		}
 	} else {
 		socket.write("invalid_ip")
 		serverInfo("invalid ip")
