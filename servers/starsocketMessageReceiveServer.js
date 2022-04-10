3dfacf795fe0bd3b9ac3489907e67a920e2ef171
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-04-10 23:01:46 +0200

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -176,7 +176,30 @@ an option to delete your account will follow soon!
 
 	
 break;
+// 4.0 about sportdash ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+case "changelog":
+	
+var about =
 
+`newest version : 0.1
+last updated : 10.04.2022
+
+
+version name :
+0.1
+
+version status :
+private
+
+update :
+[NEW] app base
+[FIX] no fixes`
+			
+//-- Save Message         		
+_messages.push(socket.remoteAddress+" "+about)
+fs.writeFileSync('./messages.json', JSON.stringify(_messages))
+	
+break;
 // 4.0 get anything ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 case "get":
 	serverInfo("new get-request")
