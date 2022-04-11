13153712ef2364505b7a66d029612176c1b446c6
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-04-11 18:28:41 +0200

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -198,6 +198,73 @@ _messages.push(socket.remoteAddress+" "+about)
 fs.writeFileSync('./messages.json', JSON.stringify(_messages))
 	
 break;
+// 4.0 terms of service sportdash ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+case "terms_of_service":
+	
+var about =
+
+`P R I V Λ C Y
+
+What can others see?
+
+other users can view your ... via your user id
+
+username
+age
+id
+level
+progress
+weight
+plans
+
+What is stored on our servers? 
+
+- - - - - 
+
+n o t e : 
+
+everything we store is for the users good only! 
+
+for example we only store your plans for you to have a Backup when you log back in and for your friends to view them
+
+no data is being sold and this will always stay that way
+
+- - - - - 
+
+After registering and agreeing to the terms and conditions, we store
+
+all the entrys you give us, such as ...
+
+username
+age
+weight
+email
+
+by using the app ...
+
+training plans
+xp
+coins
+progress
+theme
+styles
+comments
+last log in
+
+when sending feedback/reports ...
+
+when selected your hardware/software information
+after your feedback/report is reviewed it will be deleted
+
+an option to delete your account will follow soon!
+
+>_< this page is stored on our server and was last updated: 10.04.22 19:37 (CEST)`
+			
+//-- Save Message         		
+_messages.push(socket.remoteAddress+" "+about)
+fs.writeFileSync('./messages.json', JSON.stringify(_messages))
+	
+break;
 // 4.0 get anything ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 case "get":
 	serverInfo("new get-request")
