572abd03ef93941b4f5fcf6bd210371d3ff62308
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-04-14 22:02:35 +0200

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -62,16 +62,28 @@ var server = net.createServer(function(socket) {
     var receivedMessage = ""
 	var ip = socket.remoteAddress
 	var dir = "./user_messages/"+ip;
-	var user_folder = dir
+
 	if (!fs.existsSync(dir)){
 		fs.mkdirSync(dir, { recursive: true });
 		
-		fs.appendFile("./user_messages/"+ip+"messages.json", '', function (err) {
+		fs.appendFile("./user_messages/"+ip+"messages.json", '[]', function (err) {
 			if (err) throw err;
 			console.log('Saved!');
 		  });
 	}
-	var _messages = JSON.parse(fs.readFileSync("./user_messages/"+ip+"messages.json"));
+	
+	var _messages;
+	try {
+	_messages = JSON.parse(fs.readFileSync("./user_messages/"+ip+"messages.json"));
+	} catch (err){
+		fs.unlink("./user_messages/"+ip+"messages.json")
+		fs.appendFile("./user_messages/"+ip+"messages.json", '[]', function (err) {
+			if (err) throw err;
+			console.log('new messages created!');
+		  });
+	}
+
+	
 
 	socket.on('data', function(chunk) {
 		//serverInfo(`receiving message chunk...`)
