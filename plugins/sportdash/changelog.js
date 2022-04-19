32873bb81f3806bb8b6811a2972b3e6936879231
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-04-19 17:02:44 +0200

    Update changelog.js

--- plugins/sportdash/changelog.js
+++ plugins/sportdash/changelog.js
@@ -3,7 +3,7 @@ function log(){
 var changelog = `NEWVER@new app version@0.1.1
 NEW@leaderboard added functionality@0.1.1
 FIX@profile plan download crashing randomly@0.1.1
-NEWVER@new app version@0.1.
+NEWVER@new app version@0.1
 UPGRADED@first iteration of beta launch@0.1`
 
     return changelog;
