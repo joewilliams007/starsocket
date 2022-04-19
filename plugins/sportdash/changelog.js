421f3959788803ab1a1b3892fbaba301f725f480
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-04-19 21:06:09 +0200

    Update changelog.js

--- plugins/sportdash/changelog.js
+++ plugins/sportdash/changelog.js
@@ -1,7 +1,10 @@
 function log(){
 
+// NEWVER NEW FIX REMOVED ADDED UPGRADED
+
 var changelog = `NEWVER@new app version@0.1.1
-NEW@leaderboard added functionality@0.1.1
+ADDED@ability to share plan id and find plans by id@0.1.1
+UPGRADED@leaderboard added functionality@0.1.1
 FIX@profile plan download crashing randomly@0.1.1
 NEWVER@new app version@0.1
 UPGRADED@first iteration of beta launch@0.1`
