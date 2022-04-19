f6649288c6256f61c837ce8b797d50bd863d53c1
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-04-19 21:11:07 +0200

    Update changelog.js

--- plugins/sportdash/changelog.js
+++ plugins/sportdash/changelog.js
@@ -4,10 +4,10 @@ function log(){
 
 var changelog = `NEWVER@new app version@0.1.1#
 ADDED@ability to share plan id and find plans by id@0.1.1#
-UPGRADED@leaderboard added functionality@0.1.1
+UPGRADED@leaderboard added functionality
 - tap on any user to open their profile
 - new colors added
-- own user account in blue#
+- own user account in blue@0.1.1#
 FIX@profile plan download crashing randomly@0.1.1#
 NEWVER@new app version@0.1#
 UPGRADED@first iteration of beta launch@0.1#`
