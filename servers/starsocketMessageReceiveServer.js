b121267ba40021fb5a2190c606c141a9ee0e6c69
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2022-04-19 18:50:24 +0200

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -74,8 +74,16 @@ var server = net.createServer(function(socket) {
 		fs.appendFile("./user_messages/"+ip+"/messages.json", '[]', function (err) {
 			if (err) throw err;
 			console.log('new messages created!');
+			try {
 			_messages = JSON.parse(fs.readFileSync("./user_messages/"+ip+"/messages.json"));
-		  });
+		} catch (err){
+			fs.unlinkSync("./user_messages/"+ip+"/messages.json")
+			fs.appendFile("./user_messages/"+ip+"/messages.json", '[]', function (err) {
+				if (err) throw err;
+			})
+		}
+		
+		});
 		} catch (err){
 
 		}
