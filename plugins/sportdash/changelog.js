69052e9cbd4b0099230b728230a0570656bf6ded
joe <83557178+joewilliams007@users.noreply.github.com>
2022-04-24 00:03:22 +0200

    Update changelog.js

--- plugins/sportdash/changelog.js
+++ plugins/sportdash/changelog.js
@@ -12,10 +12,7 @@ NEW@Beta plan creator
 
 - search for elements online
 - upload your own elements
-- delete own uploaded elements
-- notice: 
-not complete yet
-old version remains available@0.1.2#
+- delete own uploaded elements@0.1.2#
 NEWVER@new app version@0.1.1#
 ADDED@ability to share plan id and find plans by id@0.1.1#
 UPGRADED@leaderboard added functionality
