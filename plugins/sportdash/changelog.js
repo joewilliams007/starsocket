9c385a0ef633521fa3560fd078172a6868ecd844
joe <83557178+joewilliams007@users.noreply.github.com>
2022-04-24 17:26:02 +0200

    Update changelog.js

--- plugins/sportdash/changelog.js
+++ plugins/sportdash/changelog.js
@@ -1,8 +1,9 @@
 function log(){
 
-// NEWVER NEW FIX REMOVED ADDED UPGRADED
+// NEWVER NEW FIX REMOVED ADDED UPGRADED RELEASED
 
-var changelog = `NEWVER@24.04.22 new app version@0.1.2#
+var changelog = `NEWVER@new app version@0.1.2#
+RELEASED@version date 24.04.22@0.1.2
 UPGRADED@friends profile
 
 - option to search friends by username
