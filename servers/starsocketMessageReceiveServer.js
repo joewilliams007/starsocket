e95138ff70600f4f4433f297eec3754e95450220
joe <83557178+joewilliams007@users.noreply.github.com>
2022-05-05 20:42:20 +0200

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -375,7 +375,7 @@ fs.appendFile('users/'+id+'/plan'+args[2]+'.txt', data, function (err) {
 var db =  message.split("##########");
 connection.query( // register userstuff
 `INSERT INTO Plans (plan_name, plan_description, creator_name, creator_id,plan_id, duration, category, difficulty, reports, plan_usage, plan_stars,privacy,plan_views) 
-VALUES ("${db[2]}","${db[3]}","${db[4]}","${db[5]}","${planId}",${db[7]},"${db[8]}",${db[9]}",0,0,0,1,0)`
+VALUES ("${db[2]}","${db[3]}","${db[4]}","${db[5]}","${planId}",${db[7]},"${db[8]}","${db[9]}",0,0,0,1,0)`
 , function (error, results, fields) {
 	if (error) throw error;
 });
