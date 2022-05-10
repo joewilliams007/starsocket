030a8ccda9cf6da5e908965735f56291d67daec8
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-05-10 22:03:15 +0200

    encryption added

--- plugins/sportdash/changelog.js
+++ plugins/sportdash/changelog.js
@@ -2,10 +2,14 @@ function log(){
 
 // NEWVER NEW FIX REMOVED ADDED UPGRADED RELEASED
 
-var changelog = `NEWVER@new app version@0.1.2.2#
+var changelog = 
+
+`NEWVER@new app version@0.1.2.2#
+NEW@password encryption added@0.1.2.2#
 NEW@Stars && Views of plans have been added@0.1.2.2#
-UPGRADED@"friends" page has been improved@0.1.2.2#
-NEWVER@new app version@0.1.2.1#
+UPGRADED@"friends" page has been improved@0.1.2.2#`
+
++`NEWVER@new app version@0.1.2.1#
 RELEASED@version 0.1.2 release date 5.05.22@0.1.2.1#
 UPGRADED@beta plan creator
 
@@ -13,8 +17,9 @@ UPGRADED@beta plan creator
 - better overview@0.1.2.1#
 NEW@plan preview
 
-- plan elements can now be inspected in an overview@0.1.2.1#
-NEWVER@new app version@0.1.2#
+- plan elements can now be inspected in an overview@0.1.2.1#`
+
++`NEWVER@new app version@0.1.2#
 RELEASED@version 0.1.2 release date 24.04.22@0.1.2#
 UPGRADED@friends profile
 
@@ -26,8 +31,10 @@ NEW@Beta plan creator
 
 - search for elements online
 - upload your own elements
-- delete own uploaded elements@0.1.2#
-NEWVER@new app version@0.1.1#
+- delete own uploaded elements@0.1.2#`
+
+
++`NEWVER@new app version@0.1.1#
 ADDED@ability to share plan id and find plans by id@0.1.1#
 UPGRADED@leaderboard added functionality
 
@@ -35,8 +42,9 @@ UPGRADED@leaderboard added functionality
 - new colors added
 - own user account in blue@0.1.1#
 FIX@profile plan download crashing randomly@0.1.1#
-FIX@user profile not opening with link@0.1.1#
-NEWVER@new app version@0.1#
+FIX@user profile not opening with link@0.1.1#`
+
++`NEWVER@new app version@0.1#
 UPGRADED@first iteration of beta launch@0.1`
 
     return changelog;
