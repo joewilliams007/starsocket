9d6f2c2b66aafb35f81307adbb94c0a60376c8db
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-05-12 20:43:16 +0200

    Update downloadComments.js

--- plugins/plans/comments/downloadComments.js
+++ plugins/plans/comments/downloadComments.js
@@ -27,21 +27,28 @@ function downloadComments(message, results){
     var res = JSON.parse(JSON.stringify(results))
     var position = 0;
 
- 
+    try {
 
         for (const item of res.values()) {  
             position++
+            try {
                 comments += res[position].creator_name+" #"
                 +res[position].creator_id+"@"
                 +res[position].comment+"@"
                 +res[position].comment_id+" // "
                 +res[position].date+" // "
                 +res[position].likes+"NEXTMESSAGEIS:;"
+            } catch (err) {
+             
+            }
         }
 
 
        
-   
+    } catch (err) {
+        comments = "err"
+    }
+
     return comments;
 }
 module.exports = downloadComments;
