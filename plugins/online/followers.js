c8bd5e39f2e97a2813dd8b86de2a1c39dd29f08d
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-05-14 11:21:17 +0200

    Update followers.js

--- plugins/online/followers.js
+++ plugins/online/followers.js
@@ -6,9 +6,10 @@ function follower (res) {
 					
 			for (const item of res.values()) {  
 					message+="\n"+JSON.stringify(item.follower_id)+"@"+JSON.stringify(item.follower_name)+"@"+"-";
+                 
 			}
 
-            console.log(res.values)
+            console.log(res.values())
 return message;
 }
 
