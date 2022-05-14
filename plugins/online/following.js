1eb48a4ec46297560058ba2b3d549b9f671f001a
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-05-14 11:27:28 +0200

    Update following.js

--- plugins/online/following.js
+++ plugins/online/following.js
@@ -6,11 +6,15 @@ function follower (res) {
 	var position = 0
 					
 			for (const item of res.values()) {  
+                try {
 				message+="\n"+JSON.stringify(item.follower_id)+"@"+JSON.stringify(item.follower_name)+"@"+"-";
 
                                 
                 console.log(JSON.stringify(item.follower_id))
                 console.log("-.-")
+            } catch(err){
+
+                }
 			}
 
         
