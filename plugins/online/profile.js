569d56fec7d2de9b2a86afa8d519db7d1facfe43
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-05-15 14:00:55 +0200

    Update profile.js

--- plugins/online/profile.js
+++ plugins/online/profile.js
@@ -15,17 +15,18 @@ function profile(message, results, follows, followers){
         var year = date[0]
 
 
-        profile = args[1]+" "
-        +res[0].username+" "
-        +res[0].xp+" "
-        +res[0].today_progress+" "
-        +res[0].week_progress+" "
-        +res[0].age+" "
-        +res[0].weight+" "
-        +">_<"+" "
-        +day+"."+month+"."+year+" "
-        +followers+" "
-        +follows+" "
+        profile = args[1]+"ROFILE_OF_USER"
+        +res[0].username+"ROFILE_OF_USER"
+        +res[0].xp+"ROFILE_OF_USER"
+        +res[0].today_progress+"ROFILE_OF_USER"
+        +res[0].week_progress+"ROFILE_OF_USER"
+        +res[0].age+"ROFILE_OF_USER"
+        +res[0].weight+"ROFILE_OF_USER"
+        +">_<"+"ROFILE_OF_USER"
+        +day+"."+month+"."+year+"ROFILE_OF_USER"
+        +followers+"ROFILE_OF_USER"
+        +follows+"ROFILE_OF_USER"
+        +res[0].bio+"PROFILE_OF_USER"
       //  +res[0].active_style+" "
 
        
