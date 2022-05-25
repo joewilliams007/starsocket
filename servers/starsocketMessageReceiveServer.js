738565ba16385a155cf6497e226a89e459c6a338
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-05-25 16:24:41 +0200

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -728,7 +728,7 @@ case "viewPlan":
 		const util = require('util')
 		var data = util.inspect(results, {showHidden: false, depth: null, colors: true});
 		console.log(data)
-		serverInfo("there is: "+data.RowDataPacket[0])
+		serverInfo("there is: "+data[0].RowDataPacket)
 	});
 
 	/*connection.query( // register userstuff
