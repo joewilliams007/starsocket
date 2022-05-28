044ff400dbc17cb8e5b5c36455b2e82d6f1ecea1
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-05-29 00:18:53 +0200

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -604,8 +604,8 @@ var db =  message.split("##########");
 
 var dateInSec = Math.floor(new Date() / 1000) // in seconds
 connection.query( // register userstuff
-`INSERT INTO Plans (plan_name, plan_description, creator_name, creator_id,plan_id, duration, category, difficulty, reports, plan_usage, plan_stars,privacy,plan_views,date) 
-VALUES ("${db[2]}","${db[3]}","${db[4]}","${db[5]}","${planId}",${db[7]},"${db[8]}","${db[9]}",0,0,0,1,0,${dateInSec})`
+`INSERT INTO Plans (plan_name, plan_description, creator_name, creator_id,plan_id, duration, category, difficulty, reports, plan_usage, plan_stars,privacy,plan_views,date,plan) 
+VALUES ("${db[2]}","${db[3]}","${db[4]}","${db[5]}","${planId}",${db[7]},"${db[8]}","${db[9]}",0,0,0,1,0,${dateInSec},"${data}")`
 , function (error, results, fields) {
 	if (error) throw error;
 });
@@ -657,8 +657,8 @@ var db =  message.split("##########");
 var dateInSec = Math.floor(new Date().getTime() / 1000) // in seconds
 
 connection.query( // register userstuff
-`INSERT INTO Plans (plan_name, plan_description, creator_name, creator_id,plan_id, duration, category, difficulty, reports, plan_usage, plan_stars,privacy,plan_views,date, tags) 
-VALUES ("${db[2]}","${db[3]}","${db[4]}","${db[5]}","${planId}",${db[7]},"${db[8]}","${db[9]}",0,0,0,1,0,${dateInSec},"${tags}")`
+`INSERT INTO Plans (plan_name, plan_description, creator_name, creator_id,plan_id, duration, category, difficulty, reports, plan_usage, plan_stars,privacy,plan_views,date, tags, plan) 
+VALUES ("${db[2]}","${db[3]}","${db[4]}","${db[5]}","${planId}",${db[7]},"${db[8]}","${db[9]}",0,0,0,1,0,${dateInSec},"${tags}","${data}")`
 , function (error, results, fields) {
 	if (error) throw error;
 });
