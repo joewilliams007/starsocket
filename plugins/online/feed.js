4c71ed415ed01f11646b19ac07e39bbc69b7e669
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-05-28 03:44:56 +0200

    Update feed.js

--- plugins/online/feed.js
+++ plugins/online/feed.js
@@ -6,16 +6,18 @@ function feed(type, results){
     var res = JSON.parse(JSON.stringify(results))
     var separator = "PLAN_DIVIDER"
 
-
+    try {
         if (type == "all_time") {
             feed = res[0].plan_name+separator
-            +res[0].description+separator
+            +res[0].plan_description+separator
             +res[0].tags+separator
             +res[0].plan_views+separator
             +res[0].plan_stars+separator
             +res[0].plan_id+separator
         }
-
+    } catch(err){
+        feed = "err"
+    }
 
 
 
