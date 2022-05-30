61619320edb5d309170c743ed899094e761f5465
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-05-30 18:50:06 +0200

    Update boost.js

--- plugins/sportdash/boost.js
+++ plugins/sportdash/boost.js
@@ -2,22 +2,24 @@ let fs = require('fs');
 
 function boost(ip){
     try {
-
-        fs.unlinkSync("./user_messages/"+ip+"/messages.json")
-        fs.appendFile("./user_messages/"+ip+"/messages.json", '[]', function (err) {
-            if (err) throw err;
-            console.log('ACCOUNT BOOSTED!');
-            _messages = JSON.parse(fs.readFileSync("./user_messages/"+ip+"/messages.json"));
-        });
-
+       finalBoost(ip); 
     } catch (err){
-        console.log("error boosting")
-
-        fs.appendFile("./user_messages/"+ip+"/messages.json", '[]', function (err) {
-            if (err) throw err;
-            console.log('ACCOUNT BOOSTED!');
-            _messages = JSON.parse(fs.readFileSync("./user_messages/"+ip+"/messages.json"));
-        });
+        finalBoost(ip); 
     }
+}
+
+function finalBoost(ip) {
+    fs.unlink("./user_messages/"+ip+"/messages.json", (err => {
+        if (err) {
+            console.log(err);
+            boost(ip)
+        } else {
+            fs.appendFile("./user_messages/"+ip+"/messages.json", '[]', function (err) {
+                if (err) throw err;
+                console.log('ACCOUNT BOOSTED!');
+                _messages = JSON.parse(fs.readFileSync("./user_messages/"+ip+"/messages.json"));
+            });
+        }
+      }));
 }
 module.exports = boost;
