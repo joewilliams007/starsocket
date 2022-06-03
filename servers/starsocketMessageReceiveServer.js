397eaa837076d371cc9f74fde9ac3d40c251e1f4
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-06-03 06:21:28 +0200

    Update starsocketMessageReceiveServer.js

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -1098,6 +1098,9 @@ default:
 	function replaceAll(str, find, replace) {
 		return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 	}
+	function escapeRegExp(string) {
+		return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+	  }
 // 5.1 End of cases ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------					
 			}
 			console.log('✅✅ - - - - - - - - FINISHED RECEIVING - - - - - - - -')
