e8dd938f5b72897b79d8aa8e8c71094c1c55a9ff
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-06-08 02:20:00 +0200

    up

--- servers/starsocketMessageReceiveServer.js
+++ servers/starsocketMessageReceiveServer.js
@@ -411,6 +411,33 @@ case "checkFollow":
 break;
 // send message ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 case "chat":
+	var dateInSec = Math.floor(new Date() / 1000) // in seconds
+	var FROM = user_id
+	var FROM_NAME = username;
+	var TO =  args[1]
+	var text = message.split("TEXTMESSAGESP:")[1]
+
+	if(text.length>20000) {
+		serverInfo("message is too long")
+	} else {
+		connection.query( 
+			`INSERT INTO Chats (to_id, from_id, from_name, text, type, viewed,edited,deleted,date) 
+			VALUES ("${TO}", "${FROM}","${FROM_NAME}",${text},"text",false,false,false,${dateInSec})`
+			, function (error, results, fields) {
+				if (error) throw error;
+				console.log('Yey a new chat message! >_< ');
+
+				connection.query( 
+					`INSERT INTO Notifications (user_id, from_id, viewed, date, type, notification_text,plan_id,from_name) 
+					VALUES ("${TO}", "${FROM}",false,${dateInSec},"chat"," "," ","${username}")`
+					, function (error, results, fields) {
+						if (error) throw error;
+						console.log('Yey a new notif! >_< ');
+				});
+		});
+	}
+	
+
 	sendChatMessage(message)
 break;
 // get chat ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
