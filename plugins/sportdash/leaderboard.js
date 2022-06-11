65262205e69be5deb905844293340a5ede6a7c35
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-06-11 18:36:14 +0200

    up

--- plugins/sportdash/leaderboard.js
+++ plugins/sportdash/leaderboard.js
@@ -1,16 +1,10 @@
 function leaderboard(res){
-
     result = "";
-
 					var leaderboard = "";
-					var position = 0
-					
+					var position = 0	
 					for (const item of res.values()) {  
-
 						if (Number(JSON.stringify(item.xp))<1){
-
 						} else {
-							
 						position++
 								//	console.log(`Cache item: ${JSON.stringify(item)}`)
 								if (position>100){
@@ -19,7 +13,6 @@ function leaderboard(res){
 								}
 						}
 					}
-
 					console.log(leaderboard)
 					
 return leaderboard;
